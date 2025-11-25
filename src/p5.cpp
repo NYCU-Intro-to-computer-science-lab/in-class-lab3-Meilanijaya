de15fc5b37f1e81ee12278c9073a3d45c8a086b1
Meilanijaya <meilani29.mg14@nycu.edu.tw>
2025-11-25 13:13:22 +0700

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
-
-int main() {
-    
-
+void hanoi(int n, int from, int to, int aux){
+    if (n==1){
+        cout << "Move disk 1 " << "from" << from  << "to" << to << endl;
+    } else {
+        hanoi (n-1, from, aux, to);
+        cout << "Move disk " << n << "from" << from << "to" << to << endl;
+        hanoi (n-1, aux, to, from);
+    }
+}
+int main (){
+    int n;
+    while (cin >> n){
+        hanoi (n, '1','3','2');
+    }
     return 0;
 }
