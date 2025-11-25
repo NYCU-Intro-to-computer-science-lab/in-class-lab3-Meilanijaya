3adcb3a6a44344249ada8a2ac977119428a284f9
Meilanijaya <meilani29.mg14@nycu.edu.tw>
2025-11-25 12:37:02 +0700

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+long long factorial(int n){
+    if (n==0 || n==1){
+        return 1;
+    } else {
+        return n*factorial(n-1);
+    }
+}
+
+long long sum(int n){
+    if (n==0){
+        return 0;
+    } else {
+        return n+sum(n-1);
+    }
+}
+
+int main (){
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+    cout << sum(n) << endl;
     return 0;
 }
