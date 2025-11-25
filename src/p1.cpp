911d28556457619119abf9f3b42353e0b004f6a8
Meilanijaya <meilani29.mg14@nycu.edu.tw>
2025-11-25 13:01:54 +0700

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 using namespace std;
-
 long long factorial(int n){
     if (n==0 || n==1){
         return 1;
@@ -10,7 +9,6 @@ long long factorial(int n){
         return n*factorial(n-1);
     }
 }
-
 long long sum(int n){
     if (n==0){
         return 0;
@@ -18,7 +16,6 @@ long long sum(int n){
         return n+sum(n-1);
     }
 }
-
 int main (){
     int n;
     cin >> n;
