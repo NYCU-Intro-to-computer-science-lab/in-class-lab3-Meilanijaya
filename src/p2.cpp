3a0ea5173502d584d0a176d7b17d6300a67358e1
Meilanijaya <meilani29.mg14@nycu.edu.tw>
2025-11-25 12:51:44 +0700

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -4,7 +4,29 @@
 using namespace std;
 
 int main() {
-    
-    
+
+    int gcd(int a, int b){
+    int r;
+    r= a%b;
+    a = b;
+    b = r;
+    if (r == 0){
+        return a;
+    } else {
+        return gcd(b,r);
+    }
+}
+
+int main (){
+    int a, b, r;
+    while (cin >> a >> b){
+        int r;
+        r= a%b;
+        a = b;
+        b = r;
+        cout << gcd(a,b);
+    }
     return 0;
 }
+    
+   
